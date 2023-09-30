1e35a5645de1e704a9ee269ef01cc0a623421edf
K2rlXYZ <fanbla345@gmail.com>
2023-09-30 09:34:19 +0300

    Player movement

--- Scenes/player_character.gd
+++ Scenes/player_character.gd
@@ -2,10 +2,18 @@ extends CharacterBody2D
 
 @export var movement_speed = 400 
 
+
+
 # Get user input and do something with it
-func react_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var velocity = input_direction * movement_speed
+func react_input(delta):
+	velocity += Vector2.DOWN * 10.0 * delta
+
+	velocity.x = movement_speed * (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * delta
+
+	if Input.is_action_just_pressed("ui_up"):
+		velocity = Vector2.UP * 10.0
+
+	move_and_collide(velocity)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +21,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	react_input(delta)
