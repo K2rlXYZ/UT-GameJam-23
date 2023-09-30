531953bfb2b0d6664fd187972010e75afce0e950
K2rlXYZ <fanbla345@gmail.com>
2023-09-30 13:30:45 +0300

    RAYCAST UPDATES ARE STUÜPID

--- Scenes/player_character.gd
+++ Scenes/player_character.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var movement_speed = 400*100
 @export var weight = 1
 @export var jump_speed = 5*100
+@export var sprite: Sprite2D
+@export var pickaxe: CharacterBody2D
+@export var raycast: RayCast2D
 var direction = true
 var pickaxe_x_offset = 50
 
@@ -21,28 +24,25 @@ func movement(delta):
 		jump()
 		
 	if Input.is_action_just_pressed("right"):
-		self.get_children(true).filter(func(e): return e is Sprite2D)[0].set_flip_h(true)
-		self.get_children(true).filter(func(e): return e is CharacterBody2D)[0].position.x = pickaxe_x_offset
+		sprite.set_flip_h(true)
+		pickaxe.position.x = pickaxe_x_offset
 	elif Input.is_action_just_pressed("left"):
-		self.get_children(true).filter(func(e): return e is Sprite2D)[0].set_flip_h(false)
-		self.get_children(true).filter(func(e): return e is CharacterBody2D)[0].position.x = -pickaxe_x_offset
+		sprite.set_flip_h(false)
+		pickaxe.position.x = -pickaxe_x_offset
 
 	move_and_slide()
 
 func mine():
 	var vect = get_global_mouse_position() - self.position
 	vect.limit_length(1000)
-	var raycast = self.get_children(true).filter(func(e): return e is RayCast2D)[0] as RayCast2D
 	raycast.target_position = vect
+	raycast.force_raycast_update()
 	if (raycast.is_colliding() && raycast.get_collider() is TileMap):
 		var cp = raycast.get_collision_point() as Vector2
 		var tilemap = raycast.get_collider() as TileMap
 		var local_coord = tilemap.local_to_map(tilemap.to_local(cp+vect.limit_length(20))) as Vector2i
-		tilemap.set_cell(0, local_coord, -1)
 		tilemap.erase_cell(0, local_coord)
-		tilemap.clear_layer(0)
-		
-		
+
 		
 func mine_test():
 	if Input.is_action_just_pressed("clickLeft"):
@@ -59,4 +59,4 @@ func _physics_process(delta):
 func _input(event):
 	if event is InputEventMouse:
 		if event.is_action_pressed("clickLeft"):
-			mine_test()
+			mine()
