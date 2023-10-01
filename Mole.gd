aab2d8a6565bc7acdc4ffc385674492b35e8a45c
K2rlXYZ <fanbla345@gmail.com>
2023-10-01 08:54:54 +0300

    idk

--- Mole.gd
+++ Mole.gd
@@ -29,7 +29,10 @@ func _on_area_2d_body_entered(body):
 		$mole_attack.play()
 		$Scream.play()
 		body.shoved = true
-		body.velocity.x += randf_range(2000,2500)
+		if randf() < 0.5:
+			body.velocity.x += randf_range(2000,2500)
+		else:
+			body.velocity.x -= randf_range(2000,2500)
 		body.velocity.y -= randf_range(300,900)
 		var lambda = func(empty, bod):
 			bod.velocity*=0.95
